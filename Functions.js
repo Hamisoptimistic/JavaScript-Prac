d11528e29e369f6f03fecf05d844dccb64bcfb05
Hamim <hamimbasha@outlook.com>
2023-12-25 00:02:02 +0530

    javascript main

---
+++ Functions.js
@@ -0,0 +1,15 @@
+let a = 5;
+
+function sm() {
+  a = a+1;
+  return a;
+}
+let result = sm();
+console.log (result);
+
+
+function calculateTax (parameter1,taxpercent) {
+  console.log (parameter1 * taxpercent);
+}
+calculateTax(5200,0.1);
+calculateTax(5200,0.1);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let a = 5;
+
+// function sm() {
+//   a = a+1;
+//   return a;
+// }
+// let result = sm();
+// console.log (result);
