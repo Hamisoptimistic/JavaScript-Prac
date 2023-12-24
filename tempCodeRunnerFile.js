89d7bc75e0e02e5a0c7a2fc7076cf819b6a66176
Hamim <hamimbasha@outlook.com>
2023-12-25 00:17:12 +0530

    asd

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,8 +1,8 @@
 let a = 5;
 
-// function sm() {
-//   a = a+1;
-//   return a;
-// }
-// let result = sm();
-// console.log (result);
+function sm() {
+  a = a+1;
+  return a;
+}
+let result = sm();
+console.log (result);
