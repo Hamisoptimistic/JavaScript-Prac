0aa268617fb617dc64172da5736f2253bb785469
Hamim <hamimbasha@outlook.com>
2023-12-25 16:08:19 +0530

    dfsdfsfd

--- Functions.js
+++ Functions.js
@@ -1,15 +1,28 @@
-let a = 5;
+// let a = 5;
 
-function sm() {
-  a = a+1;
-  return a;
+// function sm() {
+//   a = a+1;
+//   return a;
+// }
+// let result = sm();
+// console.log (result);
+
+
+// function calculateTax (parameter1,taxpercent) {
+//   console.log (parameter1 * taxpercent);
+// }
+// calculateTax(5200,0.1);
+// calculateTax(5200,0.1);
+
+// function addition(a, b) {
+// 	let sum = a + b;
+// 	return sum;
+// }
+// let result = addition (5, 5);
+// console.log (result);
+
+function calcAge(age) {
+	let ageindays = age*365;
+	return ageindays;
 }
-let result = sm();
-console.log (result);
-
-
-function calculateTax (parameter1,taxpercent) {
-  console.log (parameter1 * taxpercent);
-}
-calculateTax(5200,0.1);
-calculateTax(5200,0.1);
+console.log (calcAge(23));
