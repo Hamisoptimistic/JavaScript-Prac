9d4b7be54e58afe8120be6908a01efd3905bb3e8
Hamim <hamimbasha@outlook.com>
2023-12-25 17:03:33 +0530

    idnai

--- supersimplePrac/FunctionsExercises.js
+++ supersimplePrac/FunctionsExercises.js
@@ -1,12 +1,50 @@
 //7a
 
-function greet(name) {
-  if (!name) {
-    console.log ("Hi There");
+// function greet(name) {
+//   if (!name) {
+//     console.log ("Hi There");
+//   }
+//   else {
+//     console.log ('Hi ' + name);
+//   }
+// }
+
+// greet();
+
+
+function convertToFahrenheit (degree) {
+  result = '';
+  let Fahrenheit = (degree * 9/5) + 32;
+  return Fahrenheit;
+}
+
+result = convertToFahrenheit(35);
+console.log (result);
+
+function convertToCelcius (degree) {
+  let result = '';
+  let Celcius = (degree -32) * 5/9;
+  return Celcius;
+}
+
+result = convertToCelcius(110);
+console.log (result);
+
+
+
+function convertTemp(degree,unit) {
+  if (unit === 'C') {
+    let result = convertToFahrenheit(degree);
+    return result;
+    
   }
-  else {
-    console.log ('Hi ' + name);
+  else if (unit === "F") {
+    result = convertToCelcius(degree);
+    return result;
   }
 }
 
-greet();
+let conversion = convertTemp(50, 'C')
+console.log (`${conversion}` );
+
+
