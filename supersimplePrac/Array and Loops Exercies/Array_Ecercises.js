c4ced660a96ab88f60a3fb9bd90085a7754940b0
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2024-01-12 23:29:30 +0530

    asd

--- supersimplePrac/Array and Loops Exercies/Array_Ecercises.js
+++ supersimplePrac/Array and Loops Exercies/Array_Ecercises.js
@@ -1,10 +1,10 @@
 // 1.
-// let array = [10,20,30]
+let array = [10,20,30]
 
-// array[2] = 99;
-// console.log (array);
+array[2] = 99;
+console.log (array);
 
-//2. Returns Last value form an array then add 1st and last elemnent and multiply it by 2
+// 2. Returns Last value form an array then add 1st and last elemnent and multiply it by 2
 
 function get_last_Value(array) {
   let result = (array[0] + array[array.length - 1]) * 2
@@ -15,8 +15,16 @@ function get_last_Value(array) {
 let ing = get_last_Value([1,2,3,4]);
 console.log (ing)
 
-// 3. 
+// 3. Swap fist and last element
 
-// function arraySwap () {
+function arraySwap (array) {
+  let first =array[0];
+  let lastvalue = array[array.length - 1];
 
-// }
+  array[0] = lastvalue;
+  array[array.length - 1] = first;  
+  
+  return array;
+}
+
+console.log (arraySwap([1,2,3]));
---
+++ supersimplePrac/Array and Loops Exercies/Loops Exercises.js
@@ -0,0 +1,15 @@
+// 1. Loop that couunt form zero to 10 but counts up by 2
+
+// for (let i = 0; i < 11; i = i + 2) {
+//   console.log (i)
+// }
+
+// 1st loop but using while Loop
+// let i = 2;
+
+// while (i < 11) {
+//   console.log (i);
+//   i+= 2;
+// }
+
+// 
