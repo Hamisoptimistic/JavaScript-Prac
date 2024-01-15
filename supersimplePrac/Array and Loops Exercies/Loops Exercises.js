f9e220fdfbb833667bff616fefd64c174fee2891
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2024-01-15 16:49:15 +0530

    some arrys and Loops Exercies

--- supersimplePrac/Array and Loops Exercies/Loops Exercises.js
+++ supersimplePrac/Array and Loops Exercies/Loops Exercises.js
@@ -155,15 +155,37 @@
 
 // 1.Create an array of strings, loop over the array, and check if the string 'search' is inside the array. If it is, console.log() the index of 'search' in in the array. If not, console.log '-1'. •  ['hello', 'world', 'search', 'good'] => console.log(2)
 
-function searchstr(array) {
+// also using break statement
+
+// function searchstr(array) {
+//   let index = -1;
+//   for (i = 0; i < array.length; i++){
+//     if (array[i] === 'search') {
+//       index = i;
+//       break
+//     }
+    
+//   }
+//   console.log(index);
+// }
+
+// searchstr(['india','india','Hanun','search','search']);
+
+
+// Create a function findIndex(array, word) that searches an array for a string (in the 'word' parameter) and returns the index of the first appearance of the string. If it doesn't exist in the array, return -1. findIndex(['green', 'red', 'blue', 'red'], 'red') => 1 ●
+
+function findIndex(array, word) {
   let index = -1;
-  for (i = 0; i < array.length; i++){
-    if (array[i] === 'search') {
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === word) {
       index = i;
     }
-    
+    else {
+      index = 'doesnt exist'
+    }
   }
-  console.log(index);
+  return index;
+
 }
 
-searchstr(['india','india','Hanun','search']);
+console.log (findIndex(['red','green','blue'], 'white'));
