724dbcf4df89adffd13d3da35b00f78b6a8d4291
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2024-01-27 12:31:08 +0530

    asd

--- supersimplePrac/Dom Exer/practice.js
+++ supersimplePrac/Dom Exer/practice.js
@@ -1,16 +1,22 @@
 function eval() {
-let buttonValue = document.querySelector('.js-button').innerHTML = 'Loading'
+let buttonValue = document.querySelector('.js-button').innerHTML
 
 setTimeout(function() {
-  if (buttonValue === "Loading") {
+  if (buttonValue === "Start") {
     document.querySelector('.js-button').innerHTML = 'Finished'
   }
-},2000);
-
-
-
-
-
-
+},);
 console.log (buttonValue);
 }
+
+function addtoCart() {
+
+  let cartbutton = document.querySelector('.cartbutton').innerHtml
+  console.log (cartbutton);
+
+  setTimeout (function() {
+    if (cartbutton === "Add to Cart") {
+      document.querySelector('.CartButton').innerHTML = "Added"
+    }
+  },1000)
+};
