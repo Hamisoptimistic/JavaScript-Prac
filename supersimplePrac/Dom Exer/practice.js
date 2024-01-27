2d9f9ffbd1e36e967e325417b6c46f0113a1405b
Hamim <139940946+Hamisoptimistic@users.noreply.github.com>
2024-01-27 13:20:06 +0530

    xc

--- supersimplePrac/Dom Exer/practice.js
+++ supersimplePrac/Dom Exer/practice.js
@@ -19,9 +19,23 @@ function addtoCart() {
     if (cartbutton === "Add to Cart") {
       document.querySelector('.cart-button').innerHTML = "Added"
     }
-  },)
+  },1000)
 
   setTimeout (function() {
     messagevalue.innerHTML = ''
-  },2000)
+  },10000)
 };
+
+let message = 5;
+setInterval (function () {
+  if (document.title === "Practice") {
+    document.title = `${message} New Messages`}
+  // } else {
+  //   // document.title = "Practice";
+  // }
+},1000)
+
+function addremove() {
+  message++
+  message--
+}
