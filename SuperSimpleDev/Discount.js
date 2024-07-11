3780da3db7b35a8d5f915fdac0bc6273ad7674dc
Hamim <hamimbasha@outlook.com>
2024-07-11 13:13:37 +0530

    asd

--- SuperSimpleDev/Discount.js
+++
@@ -1,8 +0,0 @@
- let age =69 ;
- let Holiday = true; 
-
- if ((age < 6 || age >=65) && !Holiday)
- {
-  console.log('You are elegible for Discount');
- }
- else {console.log ('You are not elegible')}
