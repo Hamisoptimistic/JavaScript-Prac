48bd2d699cf99c3b7645f35af13b738cd049d5d1
Hamim <hamimbasha@outlook.com>
2024-07-13 23:45:52 +0530

    ifelse

---
+++ Jonas_Javascript/Assignments/if-else.js
@@ -0,0 +1,3 @@
+const indiaPop = 12;
+
+(indiaPop > 33) ? console.log(`India's population is ${indiaPop}`) : console.log(`india pop is below ${indiaPop}`)
--- Jonas_Javascript/test.js
+++ Jonas_Javascript/test.js
@@ -1,19 +1,35 @@
-// console.log("Bye World");
+// // console.log("Bye World");
+// // let myname = "Hamim"
+// // let myfav = 69;
 
-// let myname = "Hamim"
-// let myfav = 69;
+// // console.log(myfav);
+// // console.log(myname);
 
-// console.log(myfav);
-// console.log(myname);
+// // let my$name = "Hamim"
+// // console.log(my$name);
 
-// let my$name = "Hamim"
-// console.log(my$name);
+// let var1 = true;
+// console.log(typeof var1);
 
-let var1 = true;
-console.log(typeof var1);
+// const birthYear = 2000;
+// console.log(birthYear);
 
-const birthYear = 2000;
-console.log(birthYear);
+// let description = `what the fuck is this ${birthYear}`
+// console.log(description);
 
-let description = `what the fuck is this ${birthYear}`
-console.log(description);
+// let y = (2016 === 2016);
+// console.log(y);
+
+// const age = 15;
+// (age > 18) ? 'he can then' : console.log('asd');
+
+//------------------------------------------------------//
+
+
+let userInput = 'EcmaScrsipt';
+
+let result = (userInput == 'EcmaScript') ? 'correct' : 'idiot';
+
+console.log(result);
+
+
