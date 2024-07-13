84fce73d97a6f34d2aa49ce7d4f7d09569ed7ac7
Hamim <hamimbasha@outlook.com>
2024-07-14 00:22:56 +0530

    if else

--- Jonas_Javascript/Assignments/if-else.js
+++ Jonas_Javascript/Assignments/if-else.js
@@ -1,3 +1,4 @@
 const indiaPop = 12;
 
 (indiaPop > 33) ? console.log(`India's population is ${indiaPop}`) : console.log(`india pop is below ${indiaPop}`)
+
--- Jonas_Javascript/coding Challenge/Coding challenge-1.js
+++ Jonas_Javascript/coding Challenge/Coding challenge-1.js
@@ -6,10 +6,10 @@ let markHeight = 1.69;
 let johnWeight = 78;
 let johnHeight = 1.69;
 
-let BMI_mark = markWeight / (markHeight ** 2)
+let BMI_mark = markWeight / Math.round(markHeight ** 2)
 console.log(BMI_mark);
 
-let BMI_John = 28.09;
+let BMI_John = 20.09;
 
-let markHeigherBMI = BMI_John < BMI_mark;
-console.log(markHeigherBMI);
+(BMI_mark > BMI_John) ? console.log(`Mark's BMI (${BMI_mark}) is greater than John's BMI (${BMI_John})`) : console.log(`John's BMI (${BMI_John}) is greater than Mark's BMI (${BMI_mark})`)
+
