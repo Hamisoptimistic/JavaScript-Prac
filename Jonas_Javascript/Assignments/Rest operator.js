0a6e4b7aa28e6d2b171f465b9ea7950f8f84afb0
Hamim <hamimbasha@outlook.com>
2024-09-04 15:33:07 +0530

    asd

--- Jonas_Javascript/Assignments/Rest operator.js
+++ Jonas_Javascript/Assignments/Rest operator.js
@@ -1,11 +1,19 @@
 // arguments in  a function
+// using rest and spread operator at the same time
+const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const add = function (a, ...num) {
     let sum = 0;
-    console.log(a);
+    // here num is an array when we use rest operator
+    console.log(num);
     for (i = 0; i < num.length; i++) {
         sum = sum + num[i]
     }
     console.log(sum + a);
 }
-add(1, 2, 3, 4, 5, 6, 7, 8, 9,);
+add(1, ...newArr);
+
+
+
+
+
--- Jonas_Javascript/Assignments/Spread Operator.js
+++ Jonas_Javascript/Assignments/Spread Operator.js
@@ -3,4 +3,26 @@
 let fname = "hamim"
 let lname = "Basha"
 
-console.log(...fname + lname);
+// console.log(...fname + lname);
+
+const newArr = [1, 2, 3, 4, 5]
+
+function first(a, b, c) {
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+
+first(...newArr)
+
+// String example
+console.log([...fname]); // ['h', 'a', 'm', 'i', 'm']
+
+// Object example
+const obj1 = { x: 1, y: 2 };
+const obj2 = { z: 3, ...obj1 };
+console.log(obj2); // { z: 3, x: 1, y: 2 }
+
+// Set example
+const mySet = new Set([1, 2, 3]);
+console.log([...mySet]); // [1, 2, 3]
